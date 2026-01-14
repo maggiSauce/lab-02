24268b17bb9865cfb248088284aad830c09d136f
Ben B <bbui@ualberta.ca>
2026-01-13 19:16:23 -0700

    added add and remove test city functionality

--- code/app/src/main/java/com/example/listcity/MainActivity.java
+++ code/app/src/main/java/com/example/listcity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.listcity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,21 +25,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
         setContentView(R.layout.activity_main);
+
+        Button button_add_city = findViewById(R.id.button_add_city);
+        Button button_delete_city = findViewById(R.id.button_delete_city);
 
         cityList = findViewById(R.id.city_list);
 
-        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
-
+//        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+        String []cities = {"Edmonton"};
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        button_add_city.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // this is the code that is executed when the button is clicked
+                addCity("Test");
+            }
+        });
+
+        button_delete_city.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // this is the code that is executed when the button is clicked
+                deleteCity("Test");
+            }
+        });
+    }
+
+    public void addCity(String city) {
+        dataList.add(city);
+        cityList.setAdapter(cityAdapter);
+    }
+
+    public void deleteCity(String city) {
+        dataList.remove("Edmonton");
         cityList.setAdapter(cityAdapter);
     }
 }
